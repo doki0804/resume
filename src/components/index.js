8a8fe886d88bcc0ee29948793ab565ba27153297
doki0804 <doki0804@naver.com>
2026-04-26 01:32:34 +0900

    code cleanup

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,8 @@
-export { default as Navigation } from "./Navigation";
-export { default as Footer } from "./Footer";
 export { default as Contact } from "./Contact";
 export { default as AboutMe } from "./AboutMe";
 export { default as Skills } from "./Skills";
 export { default as Experience } from "./Experience";
 export { default as Languages } from "./Languages";
 export { default as Education } from "./Education";
+export { default as Footer } from "./Footer";
+export { default as FadeIn } from "./FadeIn";
